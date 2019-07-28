db2bbd0f43ec8f38f5a1aefc25fa492b6ef5cdfc
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-28 15:10:32 -0700

    added

--- instance.tf
+++ instance.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform_april.key_name}"
-    security_groups = ["${aws_security_group.sec1.id}"]
+    security_groups = ["${aws_security_group.sec1.name}"]
+
+
 
 
 
