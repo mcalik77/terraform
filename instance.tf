c7a83a0c759cf2a37cdae2cedc662eafde03470a
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-28 15:03:25 -0700

    added

--- instance.tf
+++ instance.tf
@@ -4,4 +4,14 @@ provider "aws" {
 resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
+    key_name = "${aws_key_pair.terraform}"
+    security_groups = [${aws_security_group.sec1.id}]
+    
+    tags = {
+        Name = "Dev"
+        Dept = "IT"
+        Group = "April"
+        Created_by = "Mustafa"
+    }
+
 }
---
+++ sec.group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow ssh"
+  vpc_id      = "vpc-f469098e"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+      Name = "Dev"
+      Dept = "IT"
+      Group = "April"
+      Created_by = "Mustafa"
+  }
+}
