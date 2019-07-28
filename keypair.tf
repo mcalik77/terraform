c3916448152aa117ad26e281fa2158d1013ff589
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-28 14:25:46 -0700

    added

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,4 @@
 
 resource "aws_key_pair" "terraform_april" {
     key_name = "terraform_april"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8x8sRIt0ZM+Vc2jX+KNSQbAmByIgiP6fnwJI8gCkAR/txeiEw+A0+xZ6CabP/532y6hCWZQazHwMPFl4zTpzrXUF5X1KT7UAsSIoOabSkK52P0AHIX1ivZlynuXQA0sXNEc9Etew+ajK9Oa7IjrF5Q1MnBK/6rlovs2/TmoiyVK4IQxlOnjfKK9DgbKcv/ToX0nGplRWuFWIM8fb2cylqtGGN+nX77w71jaShfYO97EYFFVcFbVZxp9Z+hGQkEfghZLq7ZxeY1iqQ26nnmL2eXvMuFmzCKGC2bBFA15OmRdIqvanqXzmrNLv4PcUD+HOJglVeTf0K1aJ2gY00rw8H root@ip-172-31-22-16.us-east-2.compute.internal"
-}
+    public_key = "${file("~/.ssh/id_rsa.pub")}"
