07c4df6e12e5621e215a03f20ce9f3d28b64ac43
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-31 17:21:46 -0700

    added

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "key_name" {
-  value = "${aws_key_pair.terraform_april}"
+  value = "${aws_key_pair.terraform_april.key_name}"
+}
+
+output "bucketname" {
+  value = "${aws_s3_bucket.b.bucket}"
 }
