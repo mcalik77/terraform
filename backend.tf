9803fa9c061abd79215002b8068044a8ce5ed46c
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-31 18:13:50 -0700

    added

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
 
     backend "s3"{
+        dynamodb_table = "dev_state_lock"
         bucket = "terraform-april-2019-mustafa"
         region = "us-east-1"
         key    = "dev.tfstate"
