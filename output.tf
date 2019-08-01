e97da94508ddd83723c3c6e5779fe0daea76c93d
Mustafa Calik <mcalikdevop@gmail.com>
2019-07-31 17:24:18 -0700

    added

--- output.tf
+++ output.tf
@@ -5,3 +5,13 @@ output "key_name" {
 output "bucketname" {
   value = "${aws_s3_bucket.b.bucket}"
 }
+output "ID" {
+  value = "${aws_instance.web1.id}"
+}
+
+output "username" {
+   value = "ec-user2"
+}
+output "public_ip" {
+   value = "${aws_instance.web1.public_ip}"
+}
