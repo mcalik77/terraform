7d629db0872cd555496c3b1bab8490f3c9ae6df8
cactus <cactus@cactuss-MacBook-Pro-2.local>
2019-09-25 09:10:23 -0700

    edited

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web1" {
-    ami = "$(var.ami)"
-    instance_type = "$(var.instance_type)"
+    ami = "ami-0b69ea66ff7391e80"
+    instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform_april.key_name}"
     security_groups = ["${aws_security_group.sec1.name}"]
     user_data = "${file("userdata.sh")}"
